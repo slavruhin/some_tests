cd0513980ff3536a10255971049b8354449044aa
Sergej <Sergej@abc-cv-explorer>
2018-03-14 17:57:36 +0100

    update

--- Solutions/src/vce/vce/Testing_100_149.java
+++ Solutions/src/vce/vce/Testing_100_149.java
@@ -1,8 +1,7 @@
 package vce.vce;
 
-import java.util.*;;
-
-
+import java.io.*;
+import java.util.*;
 
 //--------------------------------------------------------
 //test 102
@@ -21,7 +20,6 @@ class Test102 {
 		System.out.print(" doStuff x = " + x++);
 	}
 }
-
 
 //--------------------------------------------------------
 //test 103
@@ -210,10 +208,19 @@ class Test116 {
 	}
 }
 
-// --------------------------------------------------------
-// test 1XX
+
+//--------------------------------------------------------
+//test 117
 class Test117 {
+	static class Tree { }
+
 	public static void test() {
+		Test117 f = new Test117();
+		try {
+			FileOutputStream fs = new FileOutputStream("Forest.ser");
+			ObjectOutputStream os = new ObjectOutputStream(fs);
+			os.writeObject(f); os.close();
+		} catch (Exception ex) { ex.printStackTrace(); }
 	}
 }
 
@@ -222,42 +229,160 @@ class Test118 {
 	}
 }
 
+
+//--------------------------------------------------------
+//test 119
 class Test119 {
 	public static void test() {
+//		Console c = new Console();
+//		String pw;
+//		System.out.print("password: ");
+//		pw = c.readLine();
+//		System.out.println("got " + pw);
 	}
 }
 
+//--------------------------------------------------------
+//test 120
 class Test120 {
 	public static void test() {
+		double d = 12.345;
+		// insert code here
+		System.out.printf("|%7d| \n", d);
+		System.out.printf("|%7f| \n", d);
+		System.out.printf("|%3.7d| \n", d);
+		System.out.printf("|%3.7f| \n", d);
+		System.out.printf("|%7.3d| \n", d);
+		System.out.printf("|%7.3f| \n", d);
 	}
 }
 
+//--------------------------------------------------------
+//test 121
 class Test121 {
 	public static void test() {
+		String test = "Test A. Test B. Test C.";
+		//Which regular expression, inserted here, correctly splits test into "Test A", "Test B", and "Test C"?
+		//insert code here
+//		String regex = "";
+//		String regex = " ";
+//		String regex = ".*";
+//		String regex = "\\s";
+//		String regex = "\\.\\s*";
+//		String regex = "\\w[ \.] +";
+//		String[] result = test.split(regex);
 	}
 }
 
+
+//--------------------------------------------------------
+//test 122
 class Test122 {
+	interface A {
+		public void aMethod();
+	}
+
+	interface B {
+		public void bMethod();
+	}
+
+	interface C extends A, B {
+		public void cMethod();
+	}
+	static class D implements B {
+		public void bMethod() {
+		}
+	} 
+	static class E extends D implements C {
+		public void aMethod() {
+		}
+		public void bMethod() {
+		}
+		public void cMethod() {
+		}
+	}
 	public static void test() {
 	}
 }
 
+//--------------------------------------------------------
+//test 123
 class Test123 {
+	public static final String FOO = "foo";
 	public static void test() {
+		Test123 b = new Test123();
+		SubTest123 s = new SubTest123();
+		System.out.print(Test123.FOO);
+		System.out.print(SubTest123.FOO);
+		System.out.print(b.FOO);
+		System.out.print(s.FOO);
+		System.out.print(((Test123)s).FOO);
 	}
 }
+class SubTest123 extends Test123 {
+	public static final String FOO = "bar";
+} 
 
+//--------------------------------------------------------
+//test 124
 class Test124 {
+	static class Mammal {
+	}
+	static class Raccoon extends Mammal {
+		Mammal m = new Mammal();
+	}
+	static class BabyRaccoon extends Mammal {
+	}
+
 	public static void test() {
 	}
 }
 
+//--------------------------------------------------------
+//What is the result?
+//test 125
 class Test125 {
+	interface A {
+		void x();
+	}
+	static class B implements A {
+		public void x() {
+		}
+		public void y() {
+		}
+	}
+	static class C extends B {
+		public void x() {
+		}
+	}
 	public static void test() {
+		java.util.List<A> list = new java.util.ArrayList<A>();
+		list.add(new B());
+		list.add(new C());
+		for (A a : list) {
+			a.x();
+			//a.y();
+		}
 	}
 }
 
+//--------------------------------------------------------
+//test 126
 class Test126 {
+	static class Hi {
+		void m1() { }
+		protected void m2() { }
+	}
+	static class Lois extends Hi {
+		//Which four code fragments, inserted independently here, will compile? (Choose four.)
+		//public void m1() { }
+		//protected void m1() { }
+		//private void m1() { }
+		//void m2() { }
+		//public void m2() { }
+		//protected void m2() { }
+		//private void m2() { }
+	}
 	public static void test() {
 	}
 }
@@ -267,47 +392,164 @@ class Test127 {
 	}
 }
 
+//128
+//--------------------------------------------------------
+//test 128
+//What is the result?
 class Test128 {
+	static class Hello {
+		String title;
+		int value;
+		public Hello() {
+			title += " World";
+		}
+		public Hello(int value) {
+			this.value = value;
+			title = "Hello";
+//			Hello();
+		}
+	}
 	public static void test() {
+		Hello c = new Hello(5);
+		System.out.println(c.title);
 	}
 }
 
+//--------------------------------------------------------
+//test 129
 class Test129 {
+	static class Hypotenuse {
+		public InnerTriangle it = new InnerTriangle();
+
+		static class InnerTriangle {
+			public int base;
+			public int height;
+		}
+	}
 	public static void test() {
 	}
 }
 
+//130
+//--------------------------------------------------------
+//test 130
 class Test130 {
+	interface Data { public void load(); }
+	abstract class Info { public abstract void load(); }
+	
+	//Which class correctly uses the Data interface and Info class?
+	//static class Employee extends Info implements Data { public void load() { /*do something*/ }}
+	//static class Employee implements Info extends Data { public void load() { /*do something*/ }}
+	//static class Employee extends Info implements Data { public void load() { /*do something*/ } public void Info.load(){ /*do something*/ } }
+	//static class Employee implements Info extends Data { public void Data.load(){ /*do something*/ } public void load(){ /*do something*/ } }
+	//static class Employee implements Info extends Data { public void load(){ /*do something*/ } public void Info.load(){ /*do something*/ } }
+	//static class Employee extends Info implements Data{ public void Data.load() { /*do something*/ } public void Info.load() { /*do something*/ } }
+	
 	public static void test() {
 	}
 }
 
+// --------------------------------------------------------
+// test 131
+// What is the result?
 class Test131 {
 	public static void test() {
+		int []x[] = {{1,2}, {3,4,5}, {6,7,8,9}};
+		int [][]y = x;
+		System.out.println(y[2][1]);
 	}
 }
 
+//--------------------------------------------------------
+//test 132
+//What is the result?
 class Test132 {
+	static abstract class C1 {
+		public C1() {
+			System.out.print(1);
+		}
+	}
+	static class C2 extends C1 {
+		public C2() {
+			System.out.print(2);
+		}
+	}
+	static class C3 extends C2 {
+		public C3() {
+			System.out.println(3);
+		}
+	}
 	public static void test() {
+		new C3();
 	}
 }
 
+//--------------------------------------------------------
+//test 133
 class Test133 {
+	class One {
+		public One foo() {
+			return this;
+		}
+	}
+	class Two extends One {
+		public One foo() {
+			return this;
+		}
+	}
+	class Three extends Two {
+		// Which two methods, inserted individually, correctly complete the Three class? (Choose two.)
+		// public void foo() {}
+		// public int foo() { return 3; }
+		// public Two foo() { return this; }
+		//public One foo() { return this; }
+		// public Object foo() { return this; }
+	}
 	public static void test() {
 	}
 }
 
+
+//--------------------------------------------------------
+//test 134
 class Test134 {
 	public static void test() {
 	}
 }
 
+//--------------------------------------------------------
+//test 135
+// What is the result?
 class Test135 {
+	public interface A { public void m1(); }
+	
+	class C implements A { public void m1() { } }
+	//class D implements A { public void m1(int x) { } }
+	abstract class E implements A { }
+	abstract class F implements A { public void m1() { } }
+	abstract class G implements A { public void m1(int x) { } } 
+	
 	public static void test() {
 	}
 }
 
+//--------------------------------------------------------
+//test 135
 class Test136 {
+	static class Line {
+		public class Point {
+			public int x, y;
+		}
+		public Point getPoint() {
+			return new Point();
+		}
+	}
+	static class Triangle {
+		public Triangle() {
+			// Which code, inserted here, correctly retrieves a local instance of a Point object?
+			// insert code here
+		}
+	}
 	public static void test() {
 	}
 }
